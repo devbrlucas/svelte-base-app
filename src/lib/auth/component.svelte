ea1941d36e4d18baacd9309c469cd917228604b5
Lucas Moreira dos Santos <lucasmdsandradina@outlook.com>
2023-11-08 15:19:43 -0300

    Adicionada função padrão para o login do usuário

--- src/lib/auth/component.svelte
+++ src/lib/auth/component.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
     import { Input, SelectionBox } from "../form";
     import { MessagesComponent } from "../messages";
+    import { Ajax } from "../ajax";
+    import { user, type AuthResponse } from "./index";
     import "./style.less";
-    export let callback: () => Promise<void>;
-    export let email: string;
-    export let password: string;
-    export let remember: boolean;
+    import { goto } from "$app/navigation";
+    export let callback: (() => Promise<void>) | undefined = undefined;
+    export let email: string = '';
+    export let password: string = '';
+    export let remember: boolean = false;
+    type InternalAuthResponse = AuthResponse<CurrentUser['user'], 'user'>;
+    async function login(): Promise<void>
+    {
+        if (callback) return await callback();
+        const response = await Ajax
+                                    .post('/auth')
+                                    .setOption('unauthenticatedMessage', 'Dados de acesso incorretos')
+                                    .send<InternalAuthResponse>('json', {
+                                        email,
+                                        password,
+                                        remember,
+                                    });
+        if (response.error) return;
+        user.set(response.body.data, remember);
+        goto('/admin');
+    }
 </script>
 
 <main id="login-page">
@@ -13,7 +32,7 @@
         <span>Login</span>
         <img src="/logo.png" alt="logo da empresa">
     </h1>
-    <form on:submit|preventDefault={callback} id="form">
+    <form on:submit|preventDefault={login} id="form">
         <Input type="email" label="E-Mail" bind:value={email} error="email" required size="40" />
         <br>
         <Input type="password" label="Senha" bind:value={password} error="password" required size="40" />
