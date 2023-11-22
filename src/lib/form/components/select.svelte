33d9d41e0f8096403a09d4afbf209cd0f89fc890
Lucas Moreira dos Santos <lucasmdsandradina@outlook.com>
2023-11-22 14:02:49 -0300

    Correção do component form/select para funcionar com o atributo multiple

--- src/lib/form/components/select.svelte
+++ src/lib/form/components/select.svelte
@@ -5,17 +5,27 @@
         value: string | number| null | T,
         error: string,
         blank: boolean = false,
+        multiple: boolean = false,
         disabled: boolean = false;
 </script>
 
 <label class="app input-component" class:disabled>
     <span>{label}</span>
-    <select bind:value {...$$restProps} on:change autocomplete="off" {disabled}>
-        {#if blank}
-            <option value="">Selecione</option>
-        {/if}
-        <slot />
-    </select>
+    {#if multiple}
+        <select multiple bind:value {...$$restProps} on:change autocomplete="off" {disabled}>
+            {#if blank}
+                <option value="">Selecione</option>
+            {/if}
+            <slot />
+        </select>
+    {:else}
+        <select bind:value {...$$restProps} on:change autocomplete="off" {disabled}>
+            {#if blank}
+                <option value="">Selecione</option>
+            {/if}
+            <slot />
+        </select>
+    {/if}
     
     <Error name={error} />
 </label>
