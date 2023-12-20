cc5de2675af46a1d6ef8babcbc1786c44e9c1fcf
Lucas Moreira dos Santos <lucasmdsandradina@outlook.com>
2023-12-20 18:31:28 -0300

    Adição do export faltante em /filter

--- src/lib/filter/index.ts
+++ src/lib/filter/index.ts
@@ -1,6 +1,7 @@
 export { default as FilterComponent } from "./component.svelte";
 export { default as FilterButtonComponent } from "./button.svelte";
 import { filterStore } from "./store";
+export { filterStore };
 export function setFilterActive(form: Record<string, string>): void
 {
     const params = new URLSearchParams(
