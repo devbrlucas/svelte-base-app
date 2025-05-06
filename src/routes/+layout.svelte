f8aee4d58645b74c80cbb05e43193019801d27f7
Lucas Moreira dos Santos <lucas@lvsitesesistemas.com.br>
2025-05-06 09:34:03 +0000

    Atualizar versão do aplicativo para 1.2.48 em package.json e package-lock.json; ajustar estilo de ações em style.less; corrigir versão no layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,6 @@
     import { LayoutComponent } from "$lib";
 </script>
 
-<LayoutComponent version=1.2.47 baseTitle=" | Svelte Base App Test">
+<LayoutComponent version=1.2.48 baseTitle=" | Svelte Base App Test">
     <slot></slot>
 </LayoutComponent>
